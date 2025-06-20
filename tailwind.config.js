eff1d3e755a2d43bd32f0a85c3d313bc02a36d55
Adam Marek <amarek4859@gmail.com>
2025-06-20 20:52:53 +0200

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({
-    plugins: [
-        tailwindcss(),
-    ],
-})
+export default {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#1E40AF',
+            },
+        },
+    },
+    plugins: [],
+}
